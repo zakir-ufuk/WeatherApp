e9bf76bb6c2e794bd06665e2232293650ea7300b
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2023-04-24 23:14:50 +0200

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Zakir Ufuk Sahiner on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
